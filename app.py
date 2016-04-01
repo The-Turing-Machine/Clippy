d8acce6d90add4b401cfeea8e46d88bbf5649167
Arsh23 <arsh.bbps@gmail.com>
2016-04-02 02:29:23 +0530

    minor change in code

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from OpenSSL import SSL
 # context.use_privatekey_file('key.key')
 # context.use_certificate_file('key.crt')
 
+
 app = Flask(__name__)
 CORS(app)
 login_manager = LoginManager()
@@ -106,3 +107,4 @@ if __name__ == "__main__":
 
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True,port=port,host='0.0.0.0',ssl_context=('key.crt','key.key'))
+
