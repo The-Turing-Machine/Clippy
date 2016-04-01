ac01551bd25204c8159b891e01f7b9ba57312014
Arsh23 <arsh.bbps@gmail.com>
2016-04-02 02:29:23 +0530

    minor change in code

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, render_template, jsonify
+import requests
+import json
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +10,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
