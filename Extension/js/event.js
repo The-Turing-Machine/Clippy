083a1f8e5da324d95e77eb389c0138b49f830afd
thinker3197 <ashishgupta.3197@gmail.com>
2016-04-02 02:22:53 +0530

    Add event.js

---
+++ Extension/js/event.js
@@ -0,0 +1,60 @@
+(function() {
+
+    var tabs = {};
+
+
+    var inspect = {
+        activate: function(id) {
+
+            this.id = id;
+            chrome.tabs.executeScript(this.id, {
+                file: 'prism.js'
+            });
+            chrome.tabs.executeScript(this.id, {
+                file: 'hoverinspect.js'
+            }, function() {
+                chrome.tabs.sendMessage(this.id, {
+                    action: 'activate'
+                });
+            }.bind(this));
+
+            chrome.browserAction.setIcon({
+                tabId: this.id,
+                path: {
+                    19: "clippy.png"
+                }
+            });
+        },
+
+        deactivate: function() {
+
+            chrome.tabs.sendMessage(this.id, {
+                action: 'deactivate'
+            });
+
+            chrome.browserAction.setIcon({
+                tabId: this.id,
+                path: {
+                    19: "clippy.png"
+                }
+            });
+        }
+
+    };
+
+    function toggle(tab) {
+
+        if (!tabs[tab.id]) {
+            tabs[tab.id] = Object.create(inspect);
+            tabs[tab.id].activate(tab.id);
+        } else {
+            tabs[tab.id].deactivate();
+            for (var tabId in tabs) {
+                if (tabId == tab.id) delete tabs[tabId];
+            }
+        }
+    }
+
+    chrome.browserAction.onClicked.addListener(toggle);
+
+})();
