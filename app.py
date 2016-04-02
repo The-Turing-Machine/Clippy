ce0cfe374d324fe15c76d1e76ba4f656754ce2c7
Arsh23 <arsh.bbps@gmail.com>
2016-04-02 23:29:44 +0530

    login structure

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, session, g, redirect, url_for, render_template, jsonify
-
+from flask.ext.login import LoginManager, UserMixin, login_required
 import requests
 import json
 import os
@@ -7,12 +7,44 @@ json_data = {}
 
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    # user_database = json_data
+    user_database = {"admin": ("admin", "1234"),"user1": ("user1", "abcd")}
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+    @classmethod
+    def get(cls,id):
+        return cls.user_database.get(id)
+
+
+@login_manager.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+    if token is None:
+        token = request.args.get('token')
+
+    if token is not None:
+        print token
+        username,password = token.split(":") #edit this
+        user_entry = User.get(username)
+        if (user_entry is not None):
+            user = User(user_entry[0],user_entry[1])
+            if (user.password == password):
+                return user
+    return None
+
+
 
 @app.route("/")
 def hello():
     return jsonify({'status':'ok'})
 
 @app.route("/api/")
+@login_required
 def status():
     return jsonify({'status':'ok'})
 
@@ -36,7 +68,7 @@ def send(user):
 
 if __name__ == "__main__":
     global json_data
-
+    app.config["SECRET_KEY"] = "SuperDuperSecretKey!"
     with open("data/database.json") as json_file:
         json_data = json.load(json_file)
 
