fc9a38af7060ea822be0283b612443ed965b5067
Arsh23 <arsh.bbps@gmail.com>
2016-04-13 00:05:59 +0530

    user authentication added

--- app.py
+++ app.py
@@ -4,18 +4,8 @@ import requests
 import json
 import os
 from flask.ext.cors import CORS
-
-
 from pymongo import MongoClient
-
 from OpenSSL import SSL
-
-# from werkzeug.serving import make_ssl_devcert
-# make_ssl_devcert('key', host='localhost')
-
-# context = SSL.Context(SSL.SSLv23_METHOD)
-# context.use_privatekey_file('key.key')
-# context.use_certificate_file('key.crt')
 
 
 def connect():
@@ -25,16 +15,14 @@ def connect():
     handle.authenticate("admin","1234")# .......
     return handle
 
-
-
 app = Flask(__name__)
 CORS(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 db = connect()
 
-
-
+# user class
+#----------------------------------------------------------------------------------------------------
 class User(UserMixin):
     def __init__(self, username):
         data = db.collection.find({"user": username})[0]
@@ -42,12 +30,31 @@ class User(UserMixin):
         self.password = data['password']
         self.data = data['data']
 
+    def update(self,tempdata):
+        #add each item in new list to orignal list, ignore duplicates
+        db.collection.update(
+            {"user": self.user},
+            {
+                "$addToSet":
+                {
+                    "data":
+                    {
+                        "$each": tempdata['data']
+                    }
+                }
+            }
+        )
+        self.data = db.collection.find({"user": self.user})[0]['data']
+
     @classmethod
     def get(cls,uid):
         if db.collection.find({"user": uid}).limit(1).count() != 0:
             return db.collection.find({"user": uid})[0]
+#----------------------------------------------------------------------------------------------------
 
 
+# authenticates requests
+#----------------------------------------------------------------------------------------------------
 @login_manager.request_loader
 def load_user(request):
     token = request.headers.get('Authorization')
@@ -55,87 +62,49 @@ def load_user(request):
         token = request.args.get('token')
 
     if token is not None:
-        print token
         username,password = token.split(":") #edit this
         user_entry = User.get(username)
         if (user_entry is not None):
             user = User(user_entry['user'])
-            # print user.username
             if (user.password == password):
                 return user
-            # return user_entry
     return None
+#----------------------------------------------------------------------------------------------------
 
-
-
+# basic endpoints
+#----------------------------------------------------------------------------------------------------
 @app.route("/")
 def hello():
     #can be used to check connection status with api
-    return jsonify({'status':'ok'})
+    return jsonify({'status':'success'})
 
 @app.route("/api/")
 @login_required
 def status():
     #can be used to check login status
-    # return jsonify({'status':user})
-    return current_user.user
-
+    return jsonify({'status':'success','msg':'Currently logged in as '+ current_user.user})
 #----------------------------------------------------------------------------------------------------
 
 
 
 # endpoint for retriving user data from db
 #----------------------------------------------------------------------------------------------------
-@app.route("/api/get-user-data" , methods=['GET'])
+@app.route("/api/get-user-data/" , methods=['GET','POST'])
 @login_required
 def recieve():
     #retrive specific user data from cloud db
-    return jsonify({
-        'user' : current_user.user,
-        'data' : current_user.data
-    })
-
-#----------------------------------------------------------------------------------------------------
-
-
-
-# endpoint for storing user data in db
-#----------------------------------------------------------------------------------------------------
-@app.route('/api/post/', methods=['POST'])
-def send():
-    #send data to cloud db for storage
+    if current_user.is_anonymous == True:
+        return jsonify({'status':'Error','msg':'Authentication failed !!'})
     try:
-        if request.method == 'POST':
-            tempdata = json.loads(request.data)
-
-            #check if user exist in database
-            if db.collection.find({"user": tempdata['user']}).limit(1).count() == 0:
-                #user does not exist, create one
-                db.collection.insert(tempdata)
-                return jsonify({'status':'success','msg':'created-user'})
-
-            else:
-                #user exists , add each item in new list to orignal list, ignore duplicates
-                db.collection.update(
-                    {"user": tempdata['user']},
-                    {
-                        "$addToSet":
-                        {
-                            "data":
-                            {
-                                "$each": tempdata['data']
-                            }
-                        }
-                    }
-                )
-                return jsonify({'status':'success','msg':'updated-user'})
-
+        if request.method == 'GET':
+            return jsonify({
+                'user' : current_user.user,
+                'data' : current_user.data
+            })
         else:
             return jsonify({'status':'Error','msg':'That method is not allowed !!'})
-
     except Exception as e:
         return jsonify({'status':'Error','msg':'An exception has occured !! - ' + e.message})
-
 
     @app.after_request
     def after_request(response):
@@ -147,9 +116,37 @@ def send():
 
 
 
+# endpoint for storing user data in db
+#----------------------------------------------------------------------------------------------------
+@app.route('/api/post-user-data/', methods=['GET','POST'])
+@login_required
+def send():
+    #send data to cloud db for storage
+    if current_user.is_anonymous == True:
+        return jsonify({'status':'Error','msg':'Authentication failed !!'})
+    try:
+        if request.method == 'POST':
+            tempdata = json.loads(request.data)
+            current_user.update(tempdata)
+            return jsonify({'status':'success','msg':'updated-user'})
+        else:
+            return jsonify({'status':'Error','msg':'That method is not allowed !!'})
+
+    except Exception as e:
+        return jsonify({'status':'Error','msg':'An exception has occured !! - ' + e.message})
+
+    @app.after_request
+    def after_request(response):
+      response.headers.add('Access-Control-Allow-Origin', '*')
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+      return response
+#----------------------------------------------------------------------------------------------------
+
+
+#main function
 #----------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    global json_data
     app.config["SECRET_KEY"] = "SuperDuperSecretKey!"
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True,port=port,host='0.0.0.0',ssl_context=('key.crt','key.key'))
